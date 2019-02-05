8e3e93b1c76bbcb0b055d6039310d968d89bae01
Andrei Pashchenko <Andrei.p760@gmail.com>
2019-02-05 16:10:00 +0300

    1.3. JavaDoc. Code convention[#110818]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+*Class Class for calculating tasks
+*@author Andrei Pashchenko
+*@since 05.02.2019
+*@version 1
+*/
 public class Calculate {
+	/**
+	*Method main
+	*@param args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
