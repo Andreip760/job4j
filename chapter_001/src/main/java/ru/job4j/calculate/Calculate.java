197f2d33f9f1290c67868ae5be0d5ea185033dd2
Andrei Pashchenko <Andrei.p760@gmail.com>
2019-02-06 01:34:00 +0300

    2.4. Использование Git в IDEA[#110775]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,24 +1,24 @@
 package ru.job4j.calculate;
 
 /**
-*Class Class for calculating tasks
-*@author Andrei Pashchenko
-*@since 05.02.2019
-*@version 1
-*/
+ *Class for calculating tasks
+ *@author Andrei Pashchenko
+ *@since 05.02.2019
+ *@version 1
+ */
 public class Calculate {
 	/**
-	*Method main
-	*@param args
-	*/
+	 *Method main
+	 *@param args Arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	/**
-	* Method echo
-	* @param name Your name
-	* @return Echo plus your name.
-	*/
+	 * Method echo
+	 * @param name Your name
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,21 +4,21 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
-* Test
-* @author Andrei Pashchenko
-* @version $id$
-* @since 0.1
-*/
+ * Test
+ * @author Andrei Pashchenko
+ * @version $id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo
-*/
-@Test
+	/**
+	 * Test echo
+	 */
+	@Test
 	public void whenTakeNameThenThreeEchoPlusName() {
 		String input = "Andrei";
 		String expect = "Echo, echo, echo : Andrei";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
-	}	
+	}
 }
