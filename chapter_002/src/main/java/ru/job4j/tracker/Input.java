d48db97213e0c905a37ac845b5f81e50c19f5548
Andrei Pashchenko <Andrei.p760@gmail.com>
2019-02-18 09:44:35 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#110856]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+/**
+ * Input interface.
+ * @author Andrei Pashchenko.
+ * @version 1
+ * @since 18.02.2019
+ */
+public interface Input {
+    String ask(String question);
+}
