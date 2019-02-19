47e5bea7a407cfebd87aa69ddf3f1cfbaa46543d
Andrei Pashchenko <Andrei.p760@gmail.com>
2019-02-20 00:14:31 +0300

    5. Рефакторинг теста @Before @After[#110853]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -4,6 +4,8 @@ import java.util.StringJoiner;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 /**
@@ -13,14 +15,29 @@ import static org.junit.Assert.assertThat;
  * @since 19.02.2019
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    /**
+     * Redirecting output stream to byte array.
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(out));
+    }
+    /**
+     * Redirecting output stream back to console.
+     */
+    @After
+    public void backOutput() {
+        System.setOut(stdout);
+        System.out.println("execute after method");
+    }
     /**
      * Drawing a square (considering last newline as result of println)
      */
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 out.toString(),
@@ -33,16 +50,12 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
     /**
      * Drawing a triangle (considering last newline as result of println)
      */
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 out.toString(),
@@ -55,6 +68,5 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
