3253507f0a79e32513b905017e8fd49bfe7eb41d
Andrei Pashchenko <Andrei.p760@gmail.com>
2019-02-21 01:08:53 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#110854]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -78,14 +78,14 @@ public class StartUITest {
                 this.out.toString(),
                 is(
                         new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
-                        .add("Меню.")
-                        .add("0. Add new Item")
-                        .add("1. Show all items")
-                        .add("2. Edit item")
-                        .add("3. Delete item")
-                        .add("4. Find item by Id")
-                        .add("5. Find items by name")
-                        .add("6. Exit Program").toString()
+                                .add("Меню.")
+                                .add("0. Add new Item")
+                                .add("1. Show all items")
+                                .add("2. Edit item")
+                                .add("3. Delete item")
+                                .add("4. Find item by Id")
+                                .add("5. Find items by name")
+                                .add("6. Exit Program").toString()
                 )
         );
     }
